72acf089f7cb073624a584f46f08cd30d83a38c0
Chely Ho <adept_inept@yahoo.com>
2018-09-22 19:27:37 +0000

    automatically backed up by learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -13,7 +13,7 @@ export default class ChromeBoisDomain extends Component {
   }
 
   handleKeyPress = (event) => {
-    if(event.key == 'a') {resize(+)}
+    resize(+)
   }
 
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
